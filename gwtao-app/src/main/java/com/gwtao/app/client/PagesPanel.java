e5a34c5fb8e942b72cb56efdcdeeab85f5d4b288
mhueb <mhueb@pobox.com>
2012-12-26 02:54:12 +0100

    app (wip)

--- gwtao-app/src/main/java/com/gwtao/app/client/PagesPanel.java
+++ gwtao-app/src/main/java/com/gwtao/app/client/PagesPanel.java
@@ -1,16 +1,26 @@
 package com.gwtao.app.client;
 
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.gwtao.ui.util.client.SwitchPanel;
 
 public class PagesPanel implements IsWidget {
 
-  SimplePanel dummy = new SimplePanel();
+  private final SwitchPanel switchPanel = new SwitchPanel();
 
   @Override
   public Widget asWidget() {
-    return dummy;
+    return switchPanel;
+  }
+
+  public void show(IPage page) {
+    int idx = switchPanel.getWidgetIndex(page);
+    if (idx == -1) {
+      switchPanel.add(page);
+    }
+    else {
+      switchPanel.showWidget(idx);
+    }
   }
 
 }
