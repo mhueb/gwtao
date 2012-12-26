09d4bdeab0b399855d8635fac778089fc83e86d3
mhueb <mhueb@pobox.com>
2012-12-26 02:57:09 +0100

    app (wip)

--- gwtao-ui/src/main/java/com/gwtao/ui/actions/client/ActionsPanel.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/actions/client/ActionsPanel.java
@@ -1,16 +1,19 @@
 package com.gwtao.ui.actions.client;
 
-import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-public class ActionsPanel implements IsWidget {
+public class ActionsPanel extends Composite {
 
-  SimplePanel dummy = new SimplePanel();
+  private static ActionsPanelUiBinder uiBinder = GWT.create(ActionsPanelUiBinder.class);
 
-  @Override
-  public Widget asWidget() {
-    return dummy;
+  interface ActionsPanelUiBinder extends UiBinder<Widget, ActionsPanel> {
+  }
+
+  public ActionsPanel() {
+    initWidget(uiBinder.createAndBindUi(this));
   }
 
 }
--- gwtao-ui/src/main/java/com/gwtao/ui/navigator/client/NavigatorPanel.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/navigator/client/NavigatorPanel.java
@@ -1,16 +1,19 @@
 package com.gwtao.ui.navigator.client;
 
-import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-public class NavigatorPanel implements IsWidget {
+public class NavigatorPanel extends Composite {
 
-  SimplePanel dummy = new SimplePanel();
+  private static NavigatorPanelUiBinder uiBinder = GWT.create(NavigatorPanelUiBinder.class);
 
-  @Override
-  public Widget asWidget() {
-    return dummy;
+  interface NavigatorPanelUiBinder extends UiBinder<Widget, NavigatorPanel> {
+  }
+
+  public NavigatorPanel() {
+    initWidget(uiBinder.createAndBindUi(this));
   }
 
 }
