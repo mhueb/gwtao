75818c073199cff6d5c32c0881dfd0f4ca141849
mhueb <mhueb@pobox.com>
2012-12-27 20:20:15 +0100

    model/field io

---
+++ gwtao-ui/src/main/java/com/gwtao/ui/model/client/format/IntegerFormat.java
@@ -0,0 +1,37 @@
+/* 
+ * Copyright 2012 Matthias Huebner
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.gwtao.ui.model.client.format;
+
+import com.gwtao.ui.model.client.util.IValueFormat;
+import com.gwtao.ui.model.client.util.ValueParseException;
+
+public class IntegerFormat implements IValueFormat<Integer> {
+
+  public Integer parse(String text) throws ValueParseException {
+    try {
+      return Integer.parseInt(text);
+    }
+    catch (NumberFormatException e) {
+      throw new ValueParseException(e);
+    }
+  }
+
+  public String format(Integer t) {
+    if (t == null)
+      return null;
+    return t.toString();
+  }
+}
