8eba3316fbfa2365a154ba69acabb8667716ecc5
mhueb <mhueb@pobox.com>
2012-12-27 20:19:09 +0100

    fix to find folders

--- etc/genheader.cmd
+++ etc/genheader.cmd
@@ -1,13 +1,11 @@
-@ECHO OFF
+rem @ECHO OFF
 set COPYRIGHT="Copyright 2012 Matthias Huebner"
 set LICENSE="APACHE"
 
-set ROOT=%~dp0\..\
+set ROOT=%~dp0..
 set GEN="%~dp0\genheader.pl"
 
-set modules=gwtao-emul;gwtao-common;gwtao-ui;gwtao-digist;gwtao-showcase
-
-FOR %%a in (%modules%) do %GEN% -M %ROOT%\%%a\src -R -C %COPYRIGHT% -L %LICENSE% 
+FOR /f %%a in ('dir /B /A:D %ROOT%\gwtao-*') do %GEN% -M %ROOT%\%%a\src -R -C %COPYRIGHT% -L %LICENSE% 
 
 set ROOT=
 set GEN=
