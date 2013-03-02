09398ea377cf76b9d008c88aee91c278f14d9720
mhueb <mhueb@pobox.com>
2013-03-02 19:14:22 +0100

    naming

--- gwtao-ui/src/main/java/com/gwtao/ui/task/client/search/ISearchService.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/task/client/search/ISearchService.java
@@ -1,6 +1,6 @@
 package com.gwtao.ui.task.client.search;
 
-import org.shu4j.utils.search.PaginationInfo;
+import org.shu4j.utils.query.PaginationInfo;
 
 public interface ISearchService<T> {
   void search(PaginationInfo paging, T params);
