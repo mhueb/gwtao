5f4c478fd74a5868b1a5b1071faf99ced3e11b4d
mhueb <mhueb@pobox.com>
2013-06-24 01:29:48 +0200

    viewdriver refactoring - now using interface TakesValue

--- gwtao-ui/src/main/java/com/gwtao/ui/widgets/client/IDPicker.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/widgets/client/IDPicker.java
@@ -20,20 +20,21 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.text.shared.AbstractRenderer;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasConstrainedValue;
+import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.ListBox;
 import com.gwtao.ui.data.client.source.IDataSource;
 import com.gwtao.ui.data.client.source.NullDataSource;
 import com.gwtao.ui.data.client.source.events.DataChangedEvent;
 import com.gwtao.ui.data.client.source.events.DataLoadEvent;
 
-public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasConstrainedValue<T>, IsEditor<TakesValueEditor<T>> {
+public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasValue<ID>, IsEditor<TakesValueEditor<ID>> {
 
   private final List<T> values = new ArrayList<T>();
-  private final Map<ID, Integer> valueKeyToIndex = new HashMap<ID, Integer>();
+  private final Map<ID, Integer> idToIndex = new HashMap<ID, Integer>();
 
-  private TakesValueEditor<T> editor;
-  private T value;
+  private TakesValueEditor<ID> editor;
+  private T dto;
+  private ID id;
 
   private IDataSource<List<T>> source;
 
@@ -44,7 +45,6 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
   private boolean loaded;
 
   private boolean isLoading;
-  private ID id;
   private String initial;
 
   public IDPicker() {
@@ -67,42 +67,42 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
         if (selectedIndex < 0) {
           return; // Not sure why this happens during addValue
         }
-        T newValue = values.get(selectedIndex);
-        setValue(newValue, true);
+        T selectedDTO = values.get(selectedIndex);
+        setValue(selectedDTO == null ? null : selectedDTO.getId(), true);
       }
     });
   }
 
-  public HandlerRegistration addValueChangeHandler(ValueChangeHandler<T> handler) {
+  public HandlerRegistration addValueChangeHandler(ValueChangeHandler<ID> handler) {
     return addHandler(handler, ValueChangeEvent.getType());
   }
 
   /**
    * Returns a {@link TakesValueEditor} backed by the ValueListBox.
    */
-  public TakesValueEditor<T> asEditor() {
+  public TakesValueEditor<ID> asEditor() {
     if (editor == null) {
       editor = TakesValueEditor.of(this);
     }
     return editor;
   }
 
-  public T getValue() {
-    return value;
-  }
-
-  public ID getValueId() {
+  public ID getValue() {
     return id;
   }
 
-  public void setAcceptableValues(Collection<T> newValues) {
+  public T getDTO() {
+    return dto;
+  }
+
+  public void setAcceptableValues(Collection<T> dtos) {
     if (isLoading)
       return;
 
     clear();
 
-    for (T nextNewValue : newValues) {
-      addValue(nextNewValue);
+    for (T dto : dtos) {
+      addDTO(dto);
     }
 
     updateListBox();
@@ -110,7 +110,7 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
 
   private void clear() {
     values.clear();
-    valueKeyToIndex.clear();
+    idToIndex.clear();
     ListBox listBox = getListBox();
     listBox.clear();
   }
@@ -119,54 +119,54 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
    * Set the value and display it in the select element. Add the value to the acceptable set if it is not
    * already there.
    */
-  public void setValue(T value) {
+  public void setValue(ID value) {
     setValue(value, false);
   }
 
-  public void setValueById(ID id) {
+  public void setValue(ID id, boolean fireEvents) {
     if (ObjectUtils.equals(id, this.id))
       return;
 
     this.id = id;
-    this.value = null;
+    this.dto = null;
 
     if (!isLoading) {
       if (this.id != null) {
-        if (this.value == null) {
-          Integer idx = valueKeyToIndex.get(id);
+        if (this.dto == null) {
+          Integer idx = idToIndex.get(id);
           if (idx != null) {
             T t = values.get(idx);
-            setValue(t);
+            setDTO(t, fireEvents);
           }
         }
       }
       else
-        setValue(null);
+        setDTO(null, fireEvents);
     }
   }
 
-  public void setValue(T value, boolean fireEvents) {
-    if (isLoading || value == this.value || (this.value != null && this.value.equals(value))) {
+  public void setDTO(T dto, boolean fireEvents) {
+    if (isLoading || dto == this.dto || (this.dto != null && this.dto.equals(dto))) {
       return;
     }
 
-    T before = this.value;
-    this.value = value;
-    this.id = value == null ? null : value.getId();
+    ID beforeId = this.id;
+    this.id = dto == null ? null : dto.getId();
+    this.dto = dto;
     updateListBox();
 
     if (fireEvents) {
-      ValueChangeEvent.fireIfNotEqual(this, before, value);
+      ValueChangeEvent.fireIfNotEqual(this, beforeId, id);
     }
   }
 
-  private void addValue(T value) {
-    ID key = value == null ? null : value.getId();
-    if (valueKeyToIndex.containsKey(key)) {
+  private void addDTO(T value) {
+    ID id = value == null ? null : value.getId();
+    if (idToIndex.containsKey(id)) {
       throw new IllegalArgumentException("Duplicate value: " + value);
     }
 
-    valueKeyToIndex.put(key, values.size());
+    idToIndex.put(id, values.size());
     values.add(value);
     getListBox().addItem(renderer.render(value));
     assert values.size() == getListBox().getItemCount();
@@ -177,7 +177,7 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
   }
 
   private void updateListBox() {
-    ID key = value == null ? null : value.getId();
+    ID key = dto == null ? null : dto.getId();
     if (key == null && id != null)
       key = id;
 
@@ -188,20 +188,20 @@ public class IDPicker<T extends HasId<ID>, ID> extends Composite implements HasC
         String itemText = getListBox().getItemText(i);
         if (ObjectUtils.equals(itemText, initial)) {
           index = i;
-          value = values.get(index);
-          key = value.getId();
+          dto = values.get(index);
+          key = dto.getId();
           break;
         }
       }
     }
     else
-      index = valueKeyToIndex.get(key);
+      index = idToIndex.get(key);
 
     if (index == null) {
-      addValue(value);
+      addDTO(dto);
     }
 
-    index = valueKeyToIndex.get(key);
+    index = idToIndex.get(key);
     getListBox().setSelectedIndex(index);
   }
 
