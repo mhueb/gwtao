356abdddcd4411d8a48d558709689e975be17340
mhueb <mhueb@pobox.com>
2013-09-10 20:28:38 +0200

    Revert "DateTimeBox"
    This reverts commit 902a2b31e576ad8dc945288e802aaef5042ba941.

--- gwtao-ui/src/main/java/com/gwtao/ui/viewdriver/client/ViewDriver.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/viewdriver/client/ViewDriver.java
@@ -6,42 +6,31 @@ import java.util.List;
 import org.shu4j.utils.permission.Permission;
 
 import com.google.gwt.user.client.ui.Widget;
-import com.gwtao.ui.viewdriver.client.adapter.IFieldAdapter;
 
 public class ViewDriver<M> implements IViewDriver<M> {
-  private final List<IFieldAdapter<M, ? extends Widget>> fields = new ArrayList<IFieldAdapter<M, ? extends Widget>>();
+
+  private final List<WidgetAdapter<M, ?>> widgets = new ArrayList<WidgetAdapter<M, ?>>();
 
   public ViewDriver() {
   }
 
-  public <T extends Widget> void add(IFieldAdapter<M, T> adp, T field) {
-    adp.init(field);
-    fields.add(adp);
+  public <T extends Widget> void add(WidgetAdapter<M, ?> adp) {
+    widgets.add(adp);
   }
 
   @Override
   public void updateView(M model, Permission perm) {
-    for (IFieldAdapter<M, ?> field : fields) {
-      field.updateView(model);
+    for (WidgetAdapter<M, ?> widget : widgets) {
+      if (model != null)
+        widget.updateView(model);
+      widget.setPermission(perm);
     }
   }
 
   @Override
-  public void updateErrors() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void addPermissionHandler() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
   public boolean isDirty() {
-    for (IFieldAdapter<M, ?> field : fields) {
-      if (field.isDirty())
+    for (WidgetAdapter<M, ?> widget : widgets) {
+      if (widget.isDirty())
         return true;
     }
     return false;
@@ -49,15 +38,16 @@ public class ViewDriver<M> implements IViewDriver<M> {
 
   @Override
   public void clearDirty() {
-    for (IFieldAdapter<M, ?> field : fields) {
-      field.clearDirty();
+    for (WidgetAdapter<M, ?> widget : widgets) {
+      widget.clearDirty();
     }
   }
 
   @Override
   public void updateModel(M data) {
-    for (IFieldAdapter<M, ?> field : fields) {
-      field.updateModel(data);
+    for (WidgetAdapter<M, ?> widget : widgets) {
+      widget.updateModel(data);
     }
   }
+
 }
