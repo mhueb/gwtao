e3b26510844214b619e21a564dc19c4a5bfad9b3
mhueb <mhueb@pobox.com>
2013-10-15 19:37:21 +0200

    ToolsPanel/ ui:binder AddStyleName unused imports

--- gwtao-ui/src/main/java/com/gwtao/ui/cellview/client/ColumnFactory.java
+++ gwtao-ui/src/main/java/com/gwtao/ui/cellview/client/ColumnFactory.java
@@ -11,8 +11,6 @@ import com.google.gwt.cell.client.Cell;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.AbstractCellTable;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 
 public interface ColumnFactory<V> {
 
