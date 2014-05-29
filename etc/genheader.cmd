0bb2534c7208ae6d37d1fe705c554c484e13618f
mhueb <mhueb@pobox.com>
2014-05-29 23:56:39 +0200

    regexp adapter license text

--- etc/genheader.cmd
+++ etc/genheader.cmd
@@ -1,5 +1,5 @@
 rem @ECHO OFF
-set COPYRIGHT="Copyright 2012 Matthias Huebner"
+set COPYRIGHT="Copyright 2012 GWTAO"
 set LICENSE="APACHE"
 
 set ROOT=%~dp0..
--- gwtao-client/src/main/java/com/gwtao/jdk/emul/java/util/regex/Matcher.java
+++ gwtao-client/src/main/java/com/gwtao/jdk/emul/java/util/regex/Matcher.java
@@ -1,38 +1,64 @@
+/* 
+ * Copyright 2012 GWTAO
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package java.util.regex;
 
 import java.util.Objects;
 
+import org.apache.commons.lang.NotImplementedException;
+
+import com.google.gwt.regexp.shared.RegExp;
+
 public final class Matcher implements MatchResult {
+  private RegExp regexp;
+  private com.google.gwt.regexp.shared.MatchResult result;
+  
+  Matcher(String pattern, String text) {
+    this.regexp = RegExp.compile(pattern);
+    result = regexp.exec(text);
+  }
   
   public boolean matches() {
-    return false;
+    return result != null;
   }
 
   public int start() {
-    return 0;
+    return result.getIndex();
   }
 
   public int start(int group) {
-    return 0;
+    throw new NotImplementedException();
   }
 
   public int end() {
-    return 0;
+    throw new NotImplementedException();
   }
 
   public int end(int group) {
-    return 0;
+    throw new NotImplementedException();
   }
 
   public String group() {
-    return null;
+    return group(0);
   }
 
   public String group(int group) {
-    return null;
+    return result.getGroup(group);
   }
 
   public int groupCount() {
-    return 0;
+    return result.getGroupCount();
   }
 }
